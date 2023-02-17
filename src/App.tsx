4602cc5d4183f68e8c85cac5c5dbcfd79eec608b
Andrew Patipaksiri <apatipaksiri@where2getit.com>
2023-02-16 16:39:35 -0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { Button, TextField } from '@material-ui/core';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const ref = useRef<any>();
+    const [input, setInput] = useState("");
+    const [code, setCode] = useState("");
 
-export default App;
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+        });
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const handleSubmit = async () => {
+        if (!ref.current) {
+            return;
+        }
+        const result = await ref.current.build({
+            entryPoints: ["index.js"],
+            bundle: true,
+            write: false,
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(input)
+            ],
+            define: {
+                "process.env.NODE_ENV": '"production"',
+                global: "window",
+            },
+        });
+        console.log("result", result);
+        setCode(result.outputFiles[0].text);
+    };
+    return (
+        <div>
+            <div style={{ width: '600px' }}>
+                <TextField
+                    minRows={"3"}
+                    multiline
+                    fullWidth
+                    variant="outlined"
+                    onKeyDownCapture={({ ctrlKey, key = '' }) => {
+                        if (ctrlKey && key.toLowerCase() === 'enter') {
+                            handleSubmit()
+                        }
+                    }}
+                    maxRows={"7"}
+                    onChange={(e) => setInput(e.target.value)}
+                    value={input}
+                />
+            </div>
+
+
+            <div>
+                <Button color="primary" onClick={handleSubmit}>Submit</Button>
+            </div>
+            <pre>{code}</pre>
+            <iframe src="test.html" />
+        </div>
+    );
+};
+
+export default App
---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,39 @@
+import * as esbuild from "esbuild-wasm";
+
+export const unpkgPathPlugin = () => {
+    return {
+        name: "unpkg-path-plugin",
+        setup(build: esbuild.PluginBuild) {
+            build.onResolve({ filter: /(^index\.js$)/ }, () => {
+                return {
+                    path: 'index.js',
+                    namespace: 'a'
+                }
+            })
+            build.onResolve({ filter: /^\.+\// }, (args: any) => {
+                return {
+                    namespace: "a",
+                    path: new URL(
+                        args.path,
+                        `https://unpkg.com${args.resolveDir}/`
+                    ).href,
+                };
+            })
+            build.onResolve({ filter: /.*/ }, async (args: any) => {
+                return {
+                    namespace: "a",
+                    path: `https://unpkg.com/${args.path}`,
+                };
+                /*
+                  else if (args.path === "tiny-test-pkg") {
+                    return {
+                        path: "https://www.unpkg.com/tiny-test-pkg@1.0.0/index.js",
+                        namespace: "a",
+                    };
+                  }
+                */
+            });
+
+        },
+    };
+};
