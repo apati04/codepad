193cf9b8ae84309c5866a51a185d9f6283b37e1a
Andrew Patipaksiri <apatipaksiri@where2getit.com>
2023-02-23 14:39:56 -0800

    feature format button

--- src/components/code-editor/code-editor.tsx
+++ src/components/code-editor/code-editor.tsx
@@ -1,36 +1,57 @@
-import MonacoEditor from '@monaco-editor/react';
+import './code-editor-styles.css'
+import { useRef } from 'react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
+import { editorConfig } from './editor-settings';
 interface CodeEditorProps {
   initialValue: string;
   onChange(value: string): void;
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
-  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+  const editorRef = useRef<any>()
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue())
     })
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
+  }
+  const onFormatClick = () => {
+    // get current value
+
+    const value = editorRef.current?.getModel()?.getValue()
+    console.group('====[code-editor.tsx]=====');
+    console.log(': ', value);
+    console.groupEnd();
+
+    // format value
+    const formattedValue = prettier.format(value, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true
+    })
+    // set formatted value to editor
+    editorRef.current.setValue(formattedValue)
   }
   return (
-    <div>
+    <div className="editor-wrapper">
+      <button className="button button-format is-primary small" onClick={onFormatClick}>Format</button>
+
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
         theme="dark"
-        options={{
-          wordWrap: 'on',
-          minimap: { enabled: false },
-          showUnused: false,
-          folding: false,
-          lineNumbersMinChars: 3,
-          fontSize: 16,
-          scrollBeyondLastLine: false,
-          automaticLayout: true
-        }}
+        options={editorConfig.options}
         language="javascript"
         height="500px"
       />
     </div>
+
   );
 };
 
