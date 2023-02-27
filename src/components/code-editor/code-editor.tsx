c5ce6916ce0fb706a494ca98b86e8e5b00acae8f
Andrew Patipaksiri <andrew.patipak@gmail.com>
2023-02-27 14:20:20 -0800

    feature - add vertical resizing to code cell

--- src/components/code-editor/code-editor.tsx
+++ src/components/code-editor/code-editor.tsx
@@ -1,12 +1,12 @@
-import './code-editor-styles.css';
-import './syntax.css';
-import { useRef } from 'react';
-import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
-import prettier from 'prettier';
-import parser from 'prettier/parser-babel';
-import codeShift from 'jscodeshift';
-import Highlighter from 'monaco-jsx-highlighter';
-import { editorConfig } from './editor-settings';
+import "./code-editor-styles.css";
+import "./syntax.css";
+import { useRef } from "react";
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+import codeShift from "jscodeshift";
+import Highlighter from "monaco-jsx-highlighter";
+import { editorConfig } from "./editor-settings";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -14,59 +14,64 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
-  const editorRef = useRef<any>()
+  const editorRef = useRef<any>();
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
-    editorRef.current = monacoEditor
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
-      onChange(getValue())
-    })
-    monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
+      onChange(getValue());
+    });
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
     const highlighter = new Highlighter(
       // @ts-ignore
       window.monaco,
       codeShift,
       monacoEditor
-    )
+    );
     highlighter.highLightOnDidChangeModelContent(
-      () => { },
-      () => { },
+      () => {},
+      () => {},
       undefined,
-      () => { }
-    )
-  }
+      () => {}
+    );
+  };
   const onFormatClick = () => {
     // get current value
 
-    const value = editorRef.current?.getModel()?.getValue()
-    console.group('====[code-editor.tsx]=====');
-    console.log(': ', value);
+    const value = editorRef.current?.getModel()?.getValue();
+    console.group("====[code-editor.tsx]=====");
+    console.log(": ", value);
     console.groupEnd();
 
     // format value
-    const formattedValue = prettier.format(value, {
-      parser: 'babel',
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true
-    }).replace(/\n$/, '')
+    const formattedValue = prettier
+      .format(value, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, "");
     // set formatted value to editor
-    editorRef.current.setValue(formattedValue)
-  }
+    editorRef.current.setValue(formattedValue);
+  };
   return (
     <div className="editor-wrapper">
-      <button className="button button-format is-primary small" onClick={onFormatClick}>Format</button>
-
+      <button
+        className="button button-format is-primary small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
         theme="dark"
         options={editorConfig.options}
         language="javascript"
-        height="500px"
+        height="100%"
       />
     </div>
-
   );
 };
 
