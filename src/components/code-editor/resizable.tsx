6e8844f9e80d502c8ec7fbd5e5db6f4b169cf7bb
Andrew Patipaksiri <andrew.patipak@gmail.com>
2023-03-01 11:31:33 -0800

    feature resize-vertical and code preview

--- src/components/code-editor/resizable.tsx
+++ src/components/code-editor/resizable.tsx
@@ -1,6 +1,6 @@
 import "./resizable.css";
-import React from "react";
-import { ResizableBox } from "react-resizable";
+import { useEffect, useState } from "react";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 interface ResizableProps {
   direction: "horizontal" | "vertical";
@@ -8,17 +8,53 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-      minConstraints={[Infinity, 152]}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      height={300}
-      width={Infinity}
-      resizeHandles={["s"]}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+  const [viewHeight, setViewHeight] = useState(window.innerHeight);
+  const [viewWidth, setViewWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setViewHeight(window.innerHeight);
+        setViewWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      }, 100);
+    };
+    window.addEventListener("resize", listener);
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, [width]);
+
+  if (direction === "horizontal") {
+    resizableProps = {
+      className: "resize-horizontal",
+      minConstraints: [viewWidth * 0.2, Infinity],
+      maxConstraints: [viewWidth * 0.75, Infinity],
+      height: Infinity,
+      width,
+      onResizeStop: (e, data) => {
+        console.log(data);
+        setWidth(data.size.width);
+      },
+      resizeHandles: ["e"],
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, viewHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ["s"],
+    };
+  }
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
-import App from './App';
-import './index.css'
+import App from "./App";
+import "./index.css";
 const domNode = document.getElementById("root") as HTMLElement;
 const root = createRoot(domNode);
 root.render(<App />);
