ff30ed49cf596390c14df8eab1349360db819b33
Andrew Patipaksiri <andrew.patipak@gmail.com>
2023-03-16 14:22:27 -0700

    add cell reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,1 +1,33 @@
-eerreerreeeriioooiriririeoeoeorirtttttttjjjkj;
+import { Cell } from "../cell";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface CellsState {
+  loading: boolean;
+  error: string | null;
+  order: string[];
+  data: {
+    [key: string]: Cell;
+  };
+}
+const initialState: CellsState = {
+  loading: false,
+  error: null,
+  order: [],
+  data: {},
+};
+const reducer = (
+  state: CellsState = initialState,
+  action: Action
+): CellsState => {
+  switch (action.type) {
+    case ActionType.UPDATE_CELL:
+    case ActionType.DELETE_CELL:
+    case ActionType.MOVE_CELL:
+    case ActionType.INSERT_CELL_BEFORE:
+    default:
+      return state;
+  }
+};
+
+export default reducer;
