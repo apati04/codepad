3a7e0af82ef1c9fd38908e3fcdbcd55112e62bc1
Andrew Patipaksiri <andrew.patipak@gmail.com>
2023-03-22 12:28:07 -0700

    test cell creation

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { Cell } from "../cell";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
@@ -10,24 +11,55 @@ interface CellsState {
     [key: string]: Cell;
   };
 }
+
 const initialState: CellsState = {
   loading: false,
   error: null,
   order: [],
   data: {},
 };
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
+      const { id, content } = action.payload;
+      state.data[id].content = content;
+      break;
     case ActionType.DELETE_CELL:
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
+      break;
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex(() => id === action.payload.id);
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) break;
+      // swap
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      break;
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: "",
+        type: action.payload.type,
+        id: randomId(),
+      };
+      state.data[cell.id] = cell;
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+      break;
     default:
-      return state;
+      break;
   }
-};
+  return state;
+});
 
+function randomId() {
+  return Math.random().toString(36).substr(2, 5);
+}
 export default reducer;
