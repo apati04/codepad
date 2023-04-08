c2f384c7c2fe653a72c4c7f14641540121e0e18a
Andrew Patipaksiri <andrew.patipak@gmail.com>
2023-04-08 16:00:53 -0700

    feature

--- src/components/add-cell/add-cell.tsx
+++ src/components/add-cell/add-cell.tsx
@@ -3,14 +3,14 @@ import "./add-cell.css";
 
 // props: id of cell.
 interface AddCellProps {
-  nextCellId: string;
+  nextCellId: string | null;
 }
 const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useActions();
   return (
     <div>
-      <button onClick={() => insertCellBefore(nextCellId, "code")}>code</button>
-      <button onClick={() => insertCellBefore(nextCellId, "text")}>text</button>
+      <button onClick={() => insertCellBefore(nextCellId, "code")}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, "text")}>Text</button>
     </div>
   );
 };
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -30,7 +30,7 @@ export const moveCell = (
 });
 
 export const insertCellBefore = (
-  id: string,
+  id: string | null,
   cellType: CellTypes
 ): InsertCellBeforeAction => ({
   type: ActionType.INSERT_CELL_BEFORE,
