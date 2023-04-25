d88b2e8780a46fb1329f483f832afdd95847af5c
Andrew Patipaksiri <andrew.patipak@gmail.com>
2023-04-24 17:48:25 -0700

    fix

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -41,18 +41,23 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         initialValue={cell.content}
                     />
                 </Resizable>
-                {!bundle || bundle.loading ? (
-                    <div className="progress-cover">
-                        <progress
-                            className="progress is-small is-primary"
-                            max="100"
-                        >
-                            Loading
-                        </progress>
-                    </div>
-                ) : (
-                    <Preview code={bundle.code} bundlingStatus={bundle.err} />
-                )}
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className="progress-cover">
+                            <progress
+                                className="progress is-small is-primary"
+                                max="100"
+                            >
+                                Loading
+                            </progress>
+                        </div>
+                    ) : (
+                        <Preview
+                            code={bundle.code}
+                            bundlingStatus={bundle.err}
+                        />
+                    )}
+                </div>
             </div>
         </Resizable>
     );
