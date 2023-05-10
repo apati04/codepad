62441bc377e48a6dcf7c33990cfa1bacb731be21
Andrew Patipaksiri <andrew.patipak@gmail.com>
2023-05-10 13:39:02 -0700

    selective code-cell-execution (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 // import { useState } from "react";
 // import bundle from "./bundler";
-import { Provider } from "react-redux";
-import { store } from "./state";
-import CellList from "./components/code-cell/cell-list";
+import { Provider } from 'react-redux';
+import { store } from './state';
+import CellList from './components/code-cell/cell-list/cell-list';
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <div>
-        <CellList />
-      </div>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <div>
+                <CellList />
+            </div>
+        </Provider>
+    );
 };
 
 export default App;
